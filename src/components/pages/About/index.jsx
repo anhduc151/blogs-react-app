2e3ead9863885db4b5aca23d67b4246dcf9f9ab1
anhduc151 <leanhduc1510@gmail.com>
2024-01-09 16:06:55 +0700

    fix/ui

---
+++ src/components/pages/About/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './about.css'
+
+function About() {
+  return (
+    <>
+      <div className="about">
+        <div className="about_icons hover">
+          <i className="bx bxs-balloon"></i>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
